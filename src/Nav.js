b0dd658efc8750ac88f83825090caa38379be892
Nathan <nathansaccon10@hotmail.com>
2021-04-29 20:35:52 -0400

    Nav Bar updated

--- src/Nav.js
+++ src/Nav.js
@@ -4,7 +4,11 @@ import './Nav.css'
 const Nav = () => {
     return (
         <div className='nav'>
-            <img src="http://assets.stickpng.com/images/5a4fbb7bda2b4f099b95da15.png" alt="NHL logo"/>
+            <img className='nav_logo' src="http://assets.stickpng.com/images/5a4fbb7bda2b4f099b95da15.png" alt="NHL logo"/>
+            <div className="nav_pageTitles">
+                <a>Players</a>
+                <a>Teams</a>
+            </div>
         </div>
     )
 }
