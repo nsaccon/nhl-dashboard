02ae37f1f6b1a294e66104f39f713116765f240d
Nathan <nathansaccon10@hotmail.com>
2021-05-28 08:46:32 -0400

    Updated titles and logo

--- src/Header.js
+++ src/Header.js
@@ -9,8 +9,8 @@ const Header = () => {
         <source src={skaters} type="video/mp4" />
       </video>
       <div className="header_content">
-        <h1>Fancy NHL Dashboard</h1>
-        <h2>Your new site to check out hockey related css mistakes</h2>
+        <h1>Hockey Card Finder</h1>
+        <h2>Your new site for finding Young Guns cards</h2>
         <div className="header_buttons">
         <button className="header_buttonPlayer">Players</button>
         <button>Teams</button>
--- src/Nav.js
+++ src/Nav.js
@@ -4,7 +4,7 @@ import './Nav.css'
 const Nav = () => {
     return (
         <div className='nav'>
-            <img className='nav_logo' src="http://assets.stickpng.com/images/5a4fbb7bda2b4f099b95da15.png" alt="NHL logo"/>
+            <img className='nav_logo' src="https://cdn.vox-cdn.com/uploads/chorus_asset/file/16029142/EYG_MicrositeLogo.png" alt="NHL logo"/>
             <div className="nav_pageTitles">
                 <a>Players</a>
                 <a>Teams</a>
