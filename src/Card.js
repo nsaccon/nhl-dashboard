613f4b9406d8607a438e6f93b610c669abf91f24
Nathan <nathansaccon10@hotmail.com>
2021-05-29 11:31:17 -0400

    Updated search result style

--- src/Card.js
+++ src/Card.js
@@ -1,22 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Card.css";
 
-const Card = ({ cardData, distinctions  }) => {
+const Card = ({ cardData, distinctions }) => {
+  const [showCard, setShowCard] = useState(true)
+
+  function hideCard(){
+    setShowCard(false);
+  }
+
   return (
+    showCard ?
     <div className="card">
-      <h1>{cardData.title}</h1>
       <img src={cardData.imageUrl} alt="" />
-      <div className="card_price">
-        {cardData.price.toLocaleString("en-US", {
-          style: "currency",
-          currency: "USD",
-        })}{" "}
-        {cardData.currencyType}
+      <div className="card_details">
+        <h1>{cardData.title}</h1>
+        <div className="card_price">
+          {cardData.price.toLocaleString("en-US", {
+            style: "currency",
+            currency: "USD",
+          })}{" "}
+          {cardData.currencyType}
+        </div>
+        <div className="card_distinctions">
+          {cardData.distinctions.map((dist) => distinctions[dist] + " ")}
+        </div>
       </div>
-      <div className="card_distinctions">
-          {cardData.distinctions.map(dist => distinctions[dist] + " ")}
-      </div>
+      <button onClick={hideCard} className='card_removeBtn'>X</button>
     </div>
+    : <></>
   );
 };
 
